5a12039c3474ee61b5f0c517679813105e2ced1e
joshbenandiel <joshjacinto22@gmail.com>
2022-02-04 17:44:09 +0800

    Issues fix

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import "../styles/PortfolioCard.css"
 import ReactCardFlip from 'react-card-flip';
-
+import netflix from '../images/netflix.png'
+import ecommerce from '../images/ecommerce.png'
 
 const PortfolioCard = (toggleOn) => {
 
@@ -18,7 +19,7 @@ const PortfolioCard = (toggleOn) => {
       <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal" flipSpeedFrontToBack='1' flipSpeedFrontToFront='1'>
         <div className='d-flex justify-content-center align-items-center'>
           <div className={toggleOn ? "portfolio-card-light" : "portfolio-card"}>
-              
+              <img className='netflix-website' src={netflix} alt="netflix-website" />
               <i onClick={() => setHeartIsClick(!heartIsClick)} className={`bx bxs-heart bx-md ${heartIsClick ? `heart-button-active` : `heart-button`}`}></i>
               <a href="https://main.d31g18hqzwqtzt.amplifyapp.com/" target='_blank' rel="noopener noreferrer">
                <button className='btn btn-danger visit-button'>Visit Website</button>
@@ -27,7 +28,7 @@ const PortfolioCard = (toggleOn) => {
         </div>
         <div className='d-flex justify-content-center align-items-center'>
           <div className={toggleOn ? "portfolio-card-light" : "portfolio-card"}>
-         
+           <img className='ecommerce-website' src={ecommerce} alt="ecommerce-website" />
            <i onClick={() => setHeartIsClick1(!heartIsClick1)} className={`bx bxs-heart bx-md ${heartIsClick1 ? `heart-button-active` : `heart-button`}`}></i>
            <a href="https://master.d2e9splpji5fdj.amplifyapp.com/" target='_blank' rel="noopener noreferrer">
             <button  className='btn btn-danger visit-button'>Visit Website</button>
